61c04c15ffaad87013dcaebcdd829d71c66ec0b4
Krinosx <krinosx@gmail.com>
2022-05-29 00:15:56 +0200

    Adjusting build scripts

---
+++ cmakeclean.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+rm -rf ./CMakeFiles
+rm  cmake_install.cmake
+rm Makefile
+rm CMakeCache.txt
+
+rm -rf ./SDL2_GUI_Demo/CMakeFiles
+rm ./SDL2_GUI_Demo/cmake_install.cmake
+rm ./SDL2_GUI_Demo/Makefile
+rm ./SDL2_GUI_Demo/CMakeCache.txt
+
+rm -rf ./SDL2_GUI/CMakeFiles
+rm ./SDL2_GUI/cmake_install.cmake
+rm ./SDL2_GUI/Makefile
+rm ./SDL2_GUI/CMakeCache.txt
+
+rm -rf ./bin
+rm -rf ./lib
